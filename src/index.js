f7732ca53b8cf28909ff73b3b4ba21d8dc3d7400
Linacolor <annafragile123@gmail.com>
2021-10-09 15:40:28 +0300

    Добавляет решение задачи 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import refs from './refs'
+
+const { startBtnEl, stopBtnEl, bodyEl } = refs
+
 const colors = [
   '#FFFFFF',
   '#2196F3',
@@ -6,3 +10,23 @@ const colors = [
   '#009688',
   '#795548',
 ]
+startBtnEl.addEventListener('click', changeColor)
+stopBtnEl.addEventListener('click', stopChangeColor)
+// console.log(stopBtnEl)
+// console.log(bodyEl)
+
+// const randomIntegerFromInterval = (min, max) => {
+//   return Math.floor(Math.random() * (max - min + 1) + min)
+// }
+
+let intervalID = null
+function changeColor() {
+  intervalID = setInterval(() => {
+    bodyEl.style.backgroundColor =
+      colors[Math.floor(Math.random() * colors.length)]
+  }, 1000)
+}
+
+function stopChangeColor() {
+  clearInterval(intervalID)
+}
---
+++ src/refs.js
@@ -0,0 +1,5 @@
+export default {
+  startBtnEl: document.querySelector('[data-action="start"]'),
+  stopBtnEl: document.querySelector('[data-action="stop"]'),
+  bodyEl: document.getElementById('body'),
+}
